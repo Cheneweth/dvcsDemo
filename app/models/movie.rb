b8a596f93cc15e4151e3b4e62503e37cc83bf77e
Nic Cheneweth <Nic.Cheneweth@activenetwork.com>
2013-09-04 18:47:59 -0500

    setup for demo

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
 
-  validates :title, :released_on, :duration, presence: true
+  #validates :title, :released_on, :duration, presence: true
 
   validates :description, length: {minimum: 50}
 
